2b90b36613cae2ab5ce845364fe2df1a1c9ab6d8
Nadine101 <nadinet026@gmail.com>
2022-07-07 14:12:36 -0600

    frist check knowledge

---
+++ app.js
@@ -0,0 +1,9 @@
+let my_understanding = "I want to understand this so bad";
+let I_will = "I will never give up";
+let How_Many_times_I_will_try = 500;
+let Me_never_give_up_A = 999;
+
+console.log(my_understanding);
+console.log(I_will);
+console.log(How_Many_times_I_will_try);
+console.log(Me_never_give_up_A)
